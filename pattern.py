eaed7e9f9fb309b913748e0d69b21182e63cf8fa
Maheshraj04 <maheshraj6761@gmail.com>
2024-09-26 22:49:16 +0530

    Triangle code

---
+++ pattern.py
@@ -0,0 +1,25 @@
+wap right angle traingle
+n=int(input("Enter the number:"))
+
+for i in range (1,n+1):
+    print("*"*i)
+
+
+print from left to right
+n=int(input("Enter the number :"))
+for i in range(1,n+1):
+     print(" "*(n-i)+"*"*i)
+
+inverted right angle traingle
+n=int(input("Enter the number:"))
+for i in range (1,n+1):
+    print("*"*(n-i+1))
+
+pyramid pattern
+n=int(input("Entar the number:"))
+for i in range(1,n+1):
+    print((" "*(n-i)+"*"*(2*i-1)))
+
+
+   
+     
